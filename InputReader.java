9cfb45262a24cb327f62de1a3fc0f37d00082cc8
Daniel C <63077752+dcrep@users.noreply.github.com>
2023-11-26 20:03:34 -0500

    InputReader now uses a text file
    Changed InputReader to read from a text file instead of the console; also additional handling of EOF and exceptions. Added input_example.txt

--- InputReader.java
+++ InputReader.java
@@ -1,24 +1,47 @@
 import java.util.HashSet;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
 
 /**
  * InputReader reads typed text input from the standard text terminal. 
  * The text typed by a user is then chopped into words, and a set of words 
  * is provided.
  * 
- * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
+ * @author Daniel Corritore
+ *         modified from David J. Barnes and Michael Kölling.
+ * @version 2023.11.26
  */
 public class InputReader
 {
-    private Scanner reader;
+    //private Scanner readerConsole;
+    private BufferedReader readerFile;
+    private Charset charset;
+    private Path path;
+    private String filename;
 
     /**
-     * Create a new InputReader that reads text from the text terminal.
+     * Create a new InputReader that reads text from a file
+     * @throws IOException If there is an I/O error with opening a file
      */
     public InputReader()
+    throws IOException
     {
-        reader = new Scanner(System.in);
+        //readerConsole = new Scanner(System.in);
+        charset = Charset.forName("US-ASCII");
+        filename = "input_example.txt";
+        path = Paths.get(filename);
+        try {
+            readerFile = Files.newBufferedReader(path, charset);
+        }
+        catch(IOException e) {
+            System.out.println("Unable to open " + filename);
+            throw e;
+        }
     }
 
     /**
@@ -28,12 +51,68 @@ public class InputReader
      * @return  A set of Strings, where each String is one of the 
      *          words typed by the user
      */
-    public HashSet<String> getInput() 
+    /*public HashSet<String> getInputConsole() 
     {
         System.out.print("> ");                // print prompt
-        String inputLine = reader.nextLine().trim().toLowerCase();
+        String inputLine = readerConsole.nextLine().trim().toLowerCase();
 
         String[] wordArray = inputLine.split(" ");  // split at spaces
+
+        // add words from array into hashset 
+        HashSet<String> words = new HashSet<>();
+        for(String word : wordArray) {
+            words.add(word);
+        }
+        return words;
+    }*/
+    
+    /**
+     * Read a line of text from the text file and return it as a set of words.
+     * 
+     * @return A set of Strings, each containing one word of a line of text in a file
+     * @throws IOException If there is an I/O error during file reading
+     */
+    public HashSet<String> getInput()
+    throws IOException
+    {
+        boolean exitLoop = false;
+        String inputText = "";
+        try {
+            // grab a (potential) multi-line entry, combine until blank line
+            do {
+                String newLine = readerFile.readLine();
+                // end-of-file reached?
+                if (newLine == null) {
+                    exitLoop = true;
+                } else {
+                    // clear whitespace, convert to lowercase
+                    newLine = newLine.trim().toLowerCase();
+                    // empty line reached? done
+                    if (newLine.isEmpty()) {                    
+                        exitLoop = true;
+                    } else {
+                        // add a space between lines to avoid merging words
+                        if (!inputText.isEmpty())
+                            inputText += " ";
+    
+                        // and combine new line
+                        inputText += newLine;
+                    }
+                }
+            } while (!exitLoop);
+        }
+        catch (IOException e) {
+            System.out.println("Unexpected error reading file " + filename);
+        }
+        // Nothing to return
+        if (inputText.isEmpty())
+            return null;
+            
+        // Show input
+        System.out.println(">" + inputText);
+            
+        // Make the hashset
+        String[] wordArray = inputText.split(" ");  // split at spaces
 
         // add words from array into hashset 
         HashSet<String> words = new HashSet<>();
